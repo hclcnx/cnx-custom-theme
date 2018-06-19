8eb83788a223a1e8a74ede0b8328f0a3e9e1d679
Jennifer E. Heins <heinsj@us.ibm.com>
2018-06-19 15:58:56 -0400

    Initial update from 19jun2018 deploy branch

--- tmScript.js
+++ tmScript.js
@@ -5,20 +5,20 @@
 // @description  Apply visual updates to IBM Connections.
 // @author       IBM Corporation
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @require      file:///YOURFILEPATHGOESHERE/cnx-custom-theme/periscope.js
-// @resource     activities file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/activities.css
-// @resource     blogs file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/blogs.css
-// @resource     communities file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/communities.css
-// @resource     files file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/files.css
-// @resource     global file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/global.css
-// @resource     home file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/home.css
-// @resource     meetings file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/meetings.css
-// @resource     metrics file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/metrics.css
-// @resource     profiles file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/profiles.css
-// @resource     search file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/search.css
-// @resource     settings file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/settings.css
-// @resource     wikis file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/wikis.css
-// @resource     forums file:///YOURFILEPATHGOESHERE/cnx-custom-theme/css/forums.css
+// @require      file:///YOURFILEPATHGOESHERE/periscope/periscope.js
+// @resource     activities file:///YOURFILEPATHGOESHERE/periscope/css/activities.css
+// @resource     blogs file:///YOURFILEPATHGOESHERE/periscope/css/blogs.css
+// @resource     communities file:///YOURFILEPATHGOESHERE/periscope/css/communities.css
+// @resource     files file:///YOURFILEPATHGOESHERE/periscope/css/files.css
+// @resource     forums file:///YOURFILEPATHGOESHERE/periscope/css/forums.css
+// @resource     global file:///YOURFILEPATHGOESHERE/periscope/css/global.css
+// @resource     home file:///YOURFILEPATHGOESHERE/periscope/css/home.css
+// @resource     meetings file:///YOURFILEPATHGOESHERE/periscope/css/meetings.css
+// @resource     metrics file:///YOURFILEPATHGOESHERE/periscope/css/metrics.css
+// @resource     profiles file:///YOURFILEPATHGOESHERE/periscope/css/profiles.css
+// @resource     search file:///YOURFILEPATHGOESHERE/periscope/css/search.css
+// @resource     settings file:///YOURFILEPATHGOESHERE/periscope/css/settings.css
+// @resource     wikis file:///YOURFILEPATHGOESHERE/periscope/css/wikis.css
 // @match        https://apps.na.collabserv.com/activities/*
 // @match        https://apps.na.collabserv.com/blogs/*
 // @match        https://apps.na.collabserv.com/communities/*
@@ -33,7 +33,7 @@
 // @match        https://apps.na.collabserv.com/news/web/*
 // @match        https://apps.na.collabserv.com/profiles/*
 // @match        https://apps.na.collabserv.com/search/*
-// @match        https://apps.na.collabserv.com/social/home*
+// @match        https://apps.na.collabserv.com/social/*
 // @match        https://apps.na.collabserv.com/wikis/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
